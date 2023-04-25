c7109059c3c8feebc4cc398ac465861d84143fe6
vscorza0093 <vinicius.scorza@hotmail.com>
2023-04-25 02:26:37 -0300

    update?

--- set_comprehension.py
+++ set_comprehension.py
@@ -11,3 +11,6 @@
 #   It's a good way to obtain a filter effect:
 #
 #   html = {x for x in files if x.lower().endswith((".htm", ".html"))}
+
+alpha_numeric_set = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f',
+                     'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
